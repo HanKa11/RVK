3db7d625f4fa283b18bbbaf2920f47e433cabf58
Hana <hana1609@edu.zealand.dk>
2022-06-09 07:19:49 +0200

    citating

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/* java script pop up formular based on a tutorial from Online Tutorials 
+        * tutorial here:
+        * https://www.youtube.com/watch?v=bGNeZ6P3nqU
+          5.6. 2022*/
 function popupToggle(){
     const popup = document.getElementById('popup');
     popup.classList.toggle('active')
